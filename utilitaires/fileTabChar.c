b79bd939ef14bc8e9914abac7e6a0927e7402eb9
Bocar NIANG <bocar.niang@telecomnancy.eu>
2020-05-27 16:38:45 +0200

    Transform File into char array

---
+++ utilitaires/fileTabChar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include  <string.h>
+#include "fileTabChar.h"
+
+char *fileToChar(char *path) {
+
+    FILE *fich = NULL;
+    int sz = 0;
+    fich = fopen(path, "r");
+    fseek(fich, 0L, SEEK_END);
+    sz = ftell(fich);
+    char *tab = malloc(sz * sizeof(char));
+    fseek(fich, 0, SEEK_SET);
+
+    if (fich != NULL) {
+        if (tab) {
+            fread(tab, 1, sz, fich);
+        }
+        fclose(fich);
+        return tab;
+
+    }
+}
---
+++ utilitaires/fileTabChar.h
@@ -0,0 +1,9 @@
+#ifndef GROUPE7_FILETABCHAR_H
+#define GROUPE7_FILETABCHAR_H
+char *fileToChar(char *path);
+#endif //GROUPE7_FILETABCHAR_H
+
+
+
+
+

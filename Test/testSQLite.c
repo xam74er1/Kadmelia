1acf47ac55ec44b7b24271cea5db052fc5418bea
Lambert de Cursay Vincent <vcursay@laposte.net>
2020-06-03 14:57:42 +0200

    BDD sqlite II : Le retour

--- Test/testSQLite.c
+++ Test/testSQLite.c
@@ -6,16 +6,100 @@
 #include <stdlib.h>
 #include "../lib/sqlite3.h"
 
+int callback(void *, int, char **, char **);
+
 int main(int argc, char* argv[]){
     sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
-    rc = sqlite3_open("test2.db", &db);
-    if( rc ){
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        exit(0);
-    }else{
-        fprintf(stderr, "Opened database successfully\n");
+    char *err_msg = 0;
+
+    //ouverture base de donnée
+    int rc = sqlite3_open("test2.db", &db);
+
+    if (rc != SQLITE_OK) {
+
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    } else {
+        fprintf(stdout, "database opened successfully\n");
     }
+
+    //creation table
+    char *sql = "DROP TABLE IF EXISTS users;"
+                "DROP TABLE IF EXISTS titre;"
+                "DROP TABLE IF EXISTS fichier;"
+                "CREATE TABLE users(id INT,ip TEXT, port INT);"
+                "CREATE TABLE titre(hash TEXT,string TEXT);"
+                "CREATE TABLE fichier(hash TEXT, chemin_fichier TEXT);";
+
+
+    sqlite3_exec(db, sql, 0,0 ,&err_msg);
+
+    if (rc != SQLITE_OK ) {
+
+        fprintf(stderr, "Failed to create table\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    } else {
+
+        fprintf(stdout, "Tables created successfully\n");
+    }
+
+    //insertion dans table
+    sql = "INSERT INTO users(id,ip,port) VALUES (1,'122.252.185.91',8000);";
+
+    sqlite3_exec(db, sql, 0,0 ,&err_msg);
+
+    if (rc != SQLITE_OK ) {
+
+        fprintf(stderr, "Failed to insert in user table\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    } else {
+
+        fprintf(stdout, "Table user populated succefuly\n");
+    }
+
+    //Recuperation données
+    sql = "SELECT * FROM users";
+
+    sqlite3_exec(db, sql, callback, 0,&err_msg);
+
+    if (rc != SQLITE_OK ) {
+
+        fprintf(stderr, "Failed to iget user data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+
     sqlite3_close(db);
+
+    return 0;
 }
+
+//affichage de toutes les lignes des queries
+int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+
+    NotUsed = 0;
+
+    for (int i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+
+    return 0;
+}
+

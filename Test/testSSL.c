faa79fd221255c9cfd45f98a56524a9a1db53319
Lambert de Cursay Vincent <vcursay@laposte.net>
2020-06-03 12:39:27 +0200

    Sha1: ajout de l'union -> string en tableau de int

--- Test/testSSL.c
+++ Test/testSSL.c
@@ -7,18 +7,29 @@
 #include <string.h>
 //#include <openssl/sha.h>
 #include "../lib/openssl/sha.h"
+#include <inttypes.h>
+
+union mysha1{
+    uint32_t shaint[5];
+    unsigned char shachar[20];
+};
 
 int main()
 {
     const unsigned char str[] = "Original String";
-    unsigned char hash[SHA_DIGEST_LENGTH];
+    union mysha1 hash;
 
-    SHA1(str, sizeof(str) -1, hash);
+    SHA1(str, sizeof(str) -1, hash.shachar);
 
     fprintf(stderr, "Hashed successfully\n");
     fprintf(stderr, str);
-    fprintf(stderr, hash);
+    //fprintf(stderr, hash);
+    fprintf(stderr,"%" PRIu32 "\n", hash.shaint);
 
+
+  /*  for (int i = 0; i < sizeof(hash); i++)
+        fprintf(stderr, "%.3f", hash.shaint[i]);
+*/
 
     return 0;
 }

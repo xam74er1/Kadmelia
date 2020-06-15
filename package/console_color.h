ea58f8872027cf55ffa76ac092d3624c4fce5af2
Lambert de Cursay Vincent <vcursay@laposte.net>
2020-06-15 22:54:01 +0200

    Package console color!!

---
+++ package/console_color.h
@@ -0,0 +1,37 @@
+#ifndef GROUPE7_CONSOLE_COLOR_H
+#define GROUPE7_CONSOLE_COLOR_H
+
+#include <stdio.h>
+
+void clr_red ();
+
+void clr_green ();
+
+void clr_yellow ();
+
+void clr_blue ();
+
+void clr_magenta ();
+
+void clr_cyan ();
+
+
+/// Bold
+void clr_Red ();
+
+void clr_Green ();
+
+void clr_Yellow ();
+
+void clr_Blue ();
+
+void clr_Magenta ();
+
+void clr_Cyan ();
+
+
+/// reset the color
+void clr_reset ();
+
+
+#endif //GROUPE7_CONSOLE_COLOR_H

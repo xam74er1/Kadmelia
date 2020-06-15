30092d86609a7533f2cf7eef2a410d8082056fea
Lambert de Cursay Vincent <vcursay@laposte.net>
2020-06-15 22:38:50 +0200

    arguments: ip and port to main

--- Main.c
+++ Main.c
@@ -84,7 +84,49 @@ void peupleNodeMain(Node * tab[]){
  * Cette fonction me permet de fair mes test , elle est pas uen fonction propre
  *
  */
-int main() {
+int main(int argc, char **argv) {
+
+    ///////////////
+    ///Arguments///
+    ///////////////
+    int c;
+
+    while (1)
+    {
+        static struct option long_options[] =
+                {
+                        {"ip",  required_argument, 0, 'i'},
+                        {"port",  required_argument, 0, 'p'},
+                        {0, 0, 0, 0}
+                };
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "i:p:",
+                         long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+            case 'i':
+                printf ("option -i: `%s'\n", optarg);
+                break;
+
+            case 'p':
+                printf ("option -p: `%s'\n", optarg);
+                break;
+
+            default:
+                abort ();
+        }
+    }
+
+    ///////
+
+
     printf("start 2\n");
 
     srand(time(NULL));   // Initialization, du random

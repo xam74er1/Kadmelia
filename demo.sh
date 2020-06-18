ac83dd13d940b2e254a072b8ea4707f8f82b34cf
Max.Clerc <max.clerc@telecomnancy.eu>
2020-06-18 14:29:54 +0200

    hot fixe

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf nodespace
-#./kad -q 8080 -s 1 &
-#./kad -p 8080 -q 8081 -s 2 &
+#./kad -q 8080 -s 1 > demo.tmp &
+#./kad -p 8080 -q 8081 -s 2 > demo.tmp &
 #./kad -p 8080 -q 8182 -s 3 &
 
